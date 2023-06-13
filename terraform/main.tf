0c59d3a2b1ade06950aa47b3698fdac5f11ffd99
EstherVasquez <esther.vasquez@jala.university>
2023-06-13 10:00:59 -0400

    Add more space

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,8 +11,8 @@ resource "virtualbox_vm" "node" {
     count = 1
     name = format("node-%02d", count.index + 1)
     image = "https://app.vagrantup.com/ubuntu/boxes/bionic64/versions/20180903.0.0/providers/virtualbox.box"
-    cpus = 1
-    memory = "512 mib"
+    cpus = 2
+    memory = "3040 mib"
     user_data = ""
 
     network_adapter {
